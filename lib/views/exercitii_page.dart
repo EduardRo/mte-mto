404cc36e3cf92984b202bb88a7ff03a4c022813c
EduardRo <eduardstancu@yahoo.com>
2022-10-16 16:57:49 +0300

    route exercitii

--- lib/views/exercitii_page.dart
+++ lib/views/exercitii_page.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:mate_mato/utils/nav_bar.dart';
+import 'package:mate_mato/route/route.dart' as route;
 
 class ExercitiiPage extends StatelessWidget {
   const ExercitiiPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'Exercitii page',
+    return Scaffold(
+      drawer: const NavBar(),
+      appBar: AppBar(
+        title: Row(children: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add_call),
+          ),
+          const Text('Exercitii page'),
+        ]),
+        leading: IconButton(
+          onPressed: () => Navigator.pushNamed(context, route.homePage),
+          icon: const Icon(Icons.home),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.call),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.more_vert),
+          ),
+        ],
+      ),
+      body: const Center(child: Text('this is exercitii9 page')),
     );
   }
 }
