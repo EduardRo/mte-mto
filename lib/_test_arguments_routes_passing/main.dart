3bbc2b1f510dd17335c18cfa4d7f6fff5e3147b1
EduardRo <eduardstancu@yahoo.com>
2022-10-21 10:51:55 +0300

    routing test arguments

--- lib/_test_arguments_routes_passing/main.dart
+++ lib/_test_arguments_routes_passing/main.dart
@@ -1,62 +1,18 @@
 import 'package:flutter/material.dart';
+import 'page1.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: {
-        '/first': (context) => const FirstPage(),
-        '/second': (context) => const SecondPage()
-      },
-      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        /* dark theme settings */
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      // Initially display FirstPage
       home: const FirstPage(),
-    );
-  }
-}
-
-class FirstPage extends StatelessWidget {
-  const FirstPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('First Page'),
-      ),
-      body: Container(
-          child: ElevatedButton(
-        child: const Text('->Second Page'),
-        onPressed: () {},
-      )),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  const SecondPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Second Page'),
-      ),
-      body: ElevatedButton(
-        child: const Text('->first Page'),
-        onPressed: () {},
-      ),
     );
   }
 }
---
+++ lib/_test_arguments_routes_passing/page1.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'page2.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Routing App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const Text(
+              'First Page',
+              style: TextStyle(fontSize: 50),
+            ),
+            ElevatedButton(
+              child: const Text('Go to second'),
+              onPressed: () {
+                // Pushing a route directly, WITHOUT using a named route
+                Navigator.of(context).push(
+                  // With MaterialPageRoute, you can pass data between pages,
+                  // but if you have a more complex app, you will quickly get lost.
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        SecondPage(data: 'Hello there from the first page!'),
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
