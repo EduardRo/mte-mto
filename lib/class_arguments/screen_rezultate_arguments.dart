91e60b1263a03783c6eccae579b5820e364564fd
EduardRo <eduardstancu@yahoo.com>
2022-10-25 03:06:57 +0300

    merge salvare inainte de a crea argument array parametrii

---
+++ lib/class_arguments/screen_rezultate_arguments.dart
@@ -0,0 +1,9 @@
+class ScreenRezultateArguments {
+  final String r1;
+  final String r2;
+  final String r3;
+  final String r4;
+  final String r5;
+
+  ScreenRezultateArguments(this.r1, this.r2, this.r3, this.r4, this.r5);
+}
