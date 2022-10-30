8e8c087b13778d1c53752e1671d426b17bf8edc7
EduardRo <eduardstancu@yahoo.com>
2022-10-30 21:52:19 +0200

    post page add

---
+++ lib/auth_protocol/page_post.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/material.dart';
+
+class PostPage extends StatefulWidget {
+  const PostPage({super.key});
+
+  @override
+  State<PostPage> createState() => _PostPageState();
+}
+
+class _PostPageState extends State<PostPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
