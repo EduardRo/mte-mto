9213d9aee669a61526173540a7fa616ba8f7e243
EduardRo <eduardstancu@yahoo.com>
2023-01-02 19:29:13 +0200

    listview widget mod01

--- lib/ListView_widget/listview.dart
+++ lib/ListView_widget/listview.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const ListViewApp());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'ListViewWidget',
+      home: Scaffold(
+        body: ListViewApp(),
+      ),
+    );
+  }
 }
 
 class ListViewApp extends StatelessWidget {
@@ -9,20 +23,23 @@ class ListViewApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemCount: 5,
-      itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          title: const Text('Flutter Map'),
-          tileColor: Colors.orangeAccent,
-          onTap: () {},
-          leading: const Icon(Icons.person),
-          trailing: const Icon(Icons.menu),
-        );
-      },
-      separatorBuilder: ((BuildContext context, int index) => const Divider(
-            color: Colors.white,
-          )),
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: ListView.separated(
+        itemCount: 5,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: const Text('Flutter Map'),
+            tileColor: Colors.orangeAccent,
+            onTap: () {},
+            leading: const Icon(Icons.person),
+            trailing: const Icon(Icons.menu),
+          );
+        },
+        separatorBuilder: ((BuildContext context, int index) => const Divider(
+              color: Colors.white,
+            )),
+      ),
     );
   }
 }
