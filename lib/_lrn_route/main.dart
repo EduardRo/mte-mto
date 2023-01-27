61373ad9683e5496fc69a6a8b86681039e064b7d
EduardRo <eduardstancu@yahoo.com>
2023-01-27 11:00:30 +0200

    lrn-route

---
+++ lib/_lrn_route/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:mate_mato/_lrn_route/route.dart';
+import 'package:theme_provider/theme_provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ThemeProvider(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          useMaterial3: true,
+          /* dark theme settings */
+        ),
+        onGenerateRoute: RouteGenerator.controllerRoute,
+        initialRoute: '/',
+        //home: const HomePage(),
+      ),
+    );
+  }
+}
---
+++ lib/_lrn_route/route.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:mate_mato/class_arguments/screen_arguments.dart';
+
+import 'package:mate_mato/views/home_page.dart';
+import 'package:mate_mato/views/test_page.dart';
+import 'package:mate_mato/views/teste_menu_clase_page.dart';
+//import 'package:mate_mato/views/premii_page.dart';
+import 'package:mate_mato/views/rezultate_page.dart';
+import 'package:mate_mato/views/teorie_page.dart';
+import 'package:theme_provider/theme_provider.dart';
+
+import '../views/teste_menu_materii_page.dart';
+
+//import 'package:mate_mato/views/settings_page.dart';
+//import 'package:mate_mato/views/despre_page.dart';
+
+// Routes Names
+class RouteGenerator {
+// Control our page route flow
+
+  static Route<dynamic> controllerRoute(RouteSettings settings) {
+    final args = settings.arguments;
+
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (context) => const HomePage());
+
+      case '/testepage':
+        return MaterialPageRoute(
+            builder: (context) =>
+                const ThemeConsumer(child: TesteMenuClasePage()));
+      case '/rezultatepage':
+        if (args is String) {
+          return MaterialPageRoute(builder: (_) => RezultatePage(data: args));
+        } else {
+          return _errorRoute();
+        }
+
+      case '/testematerii':
+        if (args is String) {
+          return MaterialPageRoute(
+              builder: (_) => TesteMenuMateriiPage(data: args));
+        } else {
+          return _errorRoute();
+        }
+      case '/test':
+        if (args is ScreenArguments) {
+          return MaterialPageRoute(
+              builder: (_) => TestPage(
+                    args: args,
+                  ));
+        } else {
+          return _errorRoute();
+        }
+      case 'teoriepage':
+        return MaterialPageRoute(builder: (context) => const TeoriePage());
+
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('error'),
+        ),
+        body: const Center(
+          child: Text('error'),
+        ),
+      );
+    });
+  }
+}
